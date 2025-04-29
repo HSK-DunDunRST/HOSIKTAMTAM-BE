a3afd49d52e52e7b8432f635fdfbf7abf104f9ee
luna <lundreame34@gmail.com>
2025-04-29 19:04:06 +0900

    create project

---
+++ src/main/java/com/goormthonuniv/hoseo/hosiktamtam/HosiktamtamApplication.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.hoseo.hosiktamtam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosiktamtamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosiktamtamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormthonuniv/hoseo/hosiktamtam/HosiktamtamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.hoseo.hosiktamtam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosiktamtamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
